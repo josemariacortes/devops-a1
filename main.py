ff7e09d1f1a5820f3ec24522af2c74b653b6da77
josemariacortes <josemaria@cortesdiaz.com>
2026-05-31 15:33:36 +0200

    Cambiamos el número de lazamientos a 6

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ from time import sleep
 
 print(dice.__version__)
 
+amount = 6
+
 def roll(amount:int, sides:int):
     return dice.roll(f'{amount}d{sides}')
 
-for idx, result in enumerate(roll(5,6)):
+for idx, result in enumerate(roll(amount,6)):
     print(f'Lanzamiento {idx+1} número obtenido {result}')
     sleep(5)
